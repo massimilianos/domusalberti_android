d7301193e0ad3d0d4bc80a9d3f32fa6b4d1741e6
Massimiliano Saguto <massimiliano76@gmail.com>
2015-11-01 15:26:38 +0100

    Initial Commit

---
+++ DomusAlberti/src/main/java/it/max/android/domusalberti/SampleFragmentPagerAdapter.java
@@ -0,0 +1,51 @@
+package it.max.android.domusalberti;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Context;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 5;
+    private String tabTitles[] = new String[] { "1", "2", "3", "4", "5" };
+    private Context context;
+    private int[] imageResId = {
+        R.drawable.ic_one,
+        R.drawable.ic_two,
+        R.drawable.ic_three,
+        R.drawable.ic_four,
+        R.drawable.ic_five
+    };
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        //return tabTitles[position];
+        Drawable image = ContextCompat.getDrawable(context, imageResId[position]);
+        image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+        SpannableString sb = new SpannableString(" ");
+        ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;
+    }
+}
